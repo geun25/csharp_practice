a1a9f5c07fa6b408579e620e6745be61ea00e92b
gitgeun25 <78133537+gitgeun25@users.noreply.github.com>
2021-08-13 15:36:43 +0900

    Add files via upload

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public enum Operators {Add, Sub, Multi, Div }
+
+    public partial class Calculator : Form
+    {
+        public int Result = 0;
+        public bool isNewNum = true;
+        public Operators Opt = Operators.Add; // 디폴트 값
+
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        public int Add(int number1, int number2)
+        {
+            int sum = number1 + number2;
+            return sum;
+        }
+
+        public float Add(float number1, float number2)
+        {
+            float sum = number1 + number2;
+            return sum;
+        }
+
+        public int Sub(int number1, int number2)
+        {
+            int sub = number1 - number2;
+            return sub;
+        }
+
+        public int Multi(int number1, int number2)
+        {
+            int sum = number1 * number2;
+            return sum;
+        }
+
+        public void SetNum(string num)
+        {
+            if (isNewNum)
+            {
+                NumScreen.Text = num;
+                isNewNum = false;
+            }
+            else if (NumScreen.Text == "0")
+            {
+                NumScreen.Text = num;
+            }
+            else
+            {
+                NumScreen.Text = NumScreen.Text + num;
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Button numButton = (Button)sender;
+            SetNum(numButton.Text);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (isNewNum == false)
+            {
+                int num = int.Parse(NumScreen.Text);
+                if (Opt == Operators.Add)
+                    Result = Add(Result, num);
+                else if (Opt == Operators.Sub)
+                    Result = Sub(Result, num);
+                else if (Opt == Operators.Multi)
+                    Result = Multi(Result, num);
+
+                NumScreen.Text = Result.ToString();
+                isNewNum = true;
+            }
+
+            Button optButton = (Button)sender;
+
+            if (optButton.Text == "+")
+                Opt = Operators.Add;
+            else if (optButton.Text == "-")
+                Opt = Operators.Sub;
+            else if (optButton.Text == "X")
+                Opt = Operators.Multi;
+        }
+
+        private void NumClear_Click(object sender, EventArgs e)
+        {
+            Result = 0;
+            isNewNum = true;
+            Opt = Operators.Add;
+
+            NumScreen.Text = "0";
+        }
+    }
+}
