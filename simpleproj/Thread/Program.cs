bc94386f26cf5bc9f472e2bba59a3d7b69a0bf62
geun25 <sas0898@naver.com>
2021-09-05 16:51:05 +0900

    practice

---
+++ simpleproj/Thread/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+/// <summary>
+/// 순서가 보장되지 않기 때문에 결과를 명확하게 기대하기 어려움.
+/// </summary>
+namespace ThreadLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(new ThreadStart(ThreadTest));
+            thread.Start();
+
+            for(int i=0; i<6; i++)
+            {
+                Console.WriteLine($"메인스레드{i}");
+                Thread.Sleep(500);
+            }
+
+            thread.Join();
+            Console.WriteLine("스레드 종료");
+            Console.ReadLine();
+        }
+
+        static void ThreadTest()
+        {
+            for(int i =0; i<6; i++)
+            {
+                Console.WriteLine($"AddThread {i}");
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
---
+++ simpleproj/ThreadLogic2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadLogic2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] files = Directory.GetFiles(@"C:\Users\jin yeong\Desktop\C#\연습\Practice", "*.txt");
+            foreach(string file in files)
+            {
+                Thread t1 = new Thread(new ParameterizedThreadStart(Calculate));
+                t1.Start(file);
+            }
+            Console.ReadLine();
+        }
+
+        static void Calculate(object dataFile)
+        {
+            // id, 국어, 영어 합계 / 출력파일 .txt
+            string[] lines = File.ReadAllLines(dataFile.ToString());
+            List<string> lines1 = new List<string>();
+            foreach(string line in lines)
+            {
+                string id = line.Split(',')[0];
+                int korean = int.Parse(line.Split(',')[1].ToString());
+                int english = int.Parse(line.Split(',')[2].ToString());
+                int sum = korean + english;
+                lines1.Add(id + "," + korean + "," + english + "," + sum);
+            }
+            File.WriteAllLines(dataFile + ".txt", lines1);
+            string dataFile1 = dataFile.ToString().Split('\\')[7];
+            Console.WriteLine($"{DateTime.Now} : {dataFile1} - 완료");
+        }
+    }
+}
