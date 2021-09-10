ff2edafcd7ec19c39224b983f62619faa0f79465
geun25 <sas0898@naver.com>
2021-09-10 20:46:18 +0900

    Serialization

---
+++ simpleproj/Serialization/Man.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Serialization
+{
+    [Serializable]
+    class Man
+    {
+        internal string Name
+        {
+            get;
+            private set;
+        }
+
+        internal int Age
+        {
+            get;
+            private set;
+        }
+
+        internal Man(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"이름:{Name} 나이:{Age}");
+        }
+    }
+}
---
+++ simpleproj/Serialization/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+
+namespace Serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Man man = new Man("김도근", 25);
+            FileStream fs = new FileStream("man.xml", FileMode.Create);
+            //BinaryFormatter bf = new BinaryFormatter();
+            SoapFormatter sf = new SoapFormatter();
+            sf.Serialize(fs, man);
+            fs.Close();
+
+            FileStream fs2 = new FileStream("man.xml", FileMode.Open);
+            //BinaryFormatter bf2 = new BinaryFormatter();
+            SoapFormatter sf2 = new SoapFormatter();
+            Man man2 = sf2.Deserialize(fs2) as Man;
+            fs2.Close();
+            Console.WriteLine(man2);
+            Console.ReadKey();
+        }
+    }
+}
