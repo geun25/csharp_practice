821e7ec8e614c7558a62873e2c3310a493aa4a70
geun25 <sas0898@naver.com>
2021-09-10 20:45:39 +0900

    SelectiveSerialization

---
+++ simpleproj/SelectiveSerialization/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectiveSerialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Unit unit = new Unit("김도근");
+            unit.Study();
+            unit.Study();
+            unit.Study();
+            unit.Study();
+            unit.Study();
+            unit.Study();
+            unit.Study();
+            FileStream fs = new FileStream("Data.txt", FileMode.Create);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fs, unit);
+            fs.Close();
+
+            FileStream fs2 = new FileStream("Data.txt", FileMode.Open);
+            BinaryFormatter bf2 = new BinaryFormatter();
+            Unit unit2 = bf2.Deserialize(fs2) as Unit;
+            fs2.Close();
+            Console.WriteLine(unit2);
+        }
+    }
+}
---
+++ simpleproj/SelectiveSerialization/Unit.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+화 대상에서 배제
+// NonSerialized 어트리뷰트 저장                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+namespace SelectiveSerialization
+{
+    [Serializable]// 직렬화 대상으로 형식 정의
+    class Unit
+    {
+        [NonSerialized] // 유닛 개체 중에 공부한 횟수는 직렬화 대상에서 배제
+        int scnt;// 공부한 횟수
+        internal string Name
+        {
+            get;
+            private set;
+        }
+
+        internal int IQ
+        {
+            get;
+            private set;
+        }
+
+        internal Unit(string name)
+        {
+            Name = name;
+            IQ = 0;
+        }
+
+        internal void Study()
+        {
+            scnt++;//공부한 횟수 1증가
+            if (scnt == 5)//공부한 횟수가 5 되면 아이큐 1 증가 및 공부한 횟수 0으로 리셋
+            {
+                IQ++;
+                scnt = 0;
+            }
+        }
+
+        public override string ToString() // 이름, 아이큐, 공부한 시간
+        {
+            return base.ToString();
+            {
+                return string.Format($"{Name} {IQ} {scnt}");
+            }
+        }
+    }
+}
