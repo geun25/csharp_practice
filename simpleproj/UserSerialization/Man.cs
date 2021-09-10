f331e1e4edba25bbb1badcc58aadaf4d3be69a18
geun25 <sas0898@naver.com>
2021-09-10 20:46:42 +0900

    UserSerialization

---
+++ simpleproj/UserSerialization/Man.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserSerialization
+{
+    [Serializable]
+    class Man : ISerializable
+    {
+        internal string Name
+        {
+            get;
+            private set;
+        }
+
+        internal int Age
+        {
+            get;
+            private set;
+        }
+
+        internal Man(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"{Name} {Age}");
+        }
+
+        public Man(SerializationInfo info, StreamingContext context)
+        {
+            Name = info.GetString("이름");
+            Age = info.GetInt32("나이");
+        }
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("이름", Name);
+            info.AddValue("나이", Age);
+        }
+    }
+}
---
+++ simpleproj/UserSerialization/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserSerialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Man man = new Man("김도근", 25);
+            FileStream fs = new FileStream("man.xml", FileMode.Create);
+            SoapFormatter sf = new SoapFormatter();
+            sf.Serialize(fs, man);
+            fs.Close();
+            Console.WriteLine(man);
+
+            FileStream fs2 = new FileStream("man.xml", FileMode.Open);
+            SoapFormatter sf2 = new SoapFormatter();
+            Man man2 = sf.Deserialize(fs2) as Man;
+            fs2.Close();
+            Console.WriteLine(man2);
+        }
+    }
+}
