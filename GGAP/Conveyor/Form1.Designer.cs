f3ef9c04557dada92717e0fe97361e3caa50b432
geun25 <sas0898@naver.com>
2021-11-28 20:41:44 +0900

    211128

---
+++ GGAP/Conveyor/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿
+namespace Conveyor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cbSensor1_1 = new System.Windows.Forms.CheckBox();
+            this.cbSensor1_2 = new System.Windows.Forms.CheckBox();
+            this.cbSensor2_2 = new System.Windows.Forms.CheckBox();
+            this.cbSensor2_1 = new System.Windows.Forms.CheckBox();
+            this.cbSensor4_2 = new System.Windows.Forms.CheckBox();
+            this.cbSensor4_1 = new System.Windows.Forms.CheckBox();
+            this.cbSensor3_2 = new System.Windows.Forms.CheckBox();
+            this.cbSensor3_1 = new System.Windows.Forms.CheckBox();
+            this.cbSensorS_2 = new System.Windows.Forms.CheckBox();
+            this.cbSensorS_1 = new System.Windows.Forms.CheckBox();
+            this.btnConveyor3 = new System.Windows.Forms.Button();
+            this.btnConveyor4 = new System.Windows.Forms.Button();
+            this.btnConveyorS = new System.Windows.Forms.Button();
+            this.btnConveyor1 = new System.Windows.Forms.Button();
+            this.btnConveyor2 = new System.Windows.Forms.Button();
+            this.btnAuto = new System.Windows.Forms.Button();
+            this.btnTakeOut = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnTakeIn = new System.Windows.Forms.Button();
+            this.ConvMotionProc = new System.Windows.Forms.Timer(this.components);
+            this.MainSchedulerProc = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // cbSensor1_1
+            // 
+            this.cbSensor1_1.AutoSize = true;
+            this.cbSensor1_1.Location = new System.Drawing.Point(76, 333);
+            this.cbSensor1_1.Name = "cbSensor1_1";
+            this.cbSensor1_1.Size = new System.Drawing.Size(53, 19);
+            this.cbSensor1_1.TabIndex = 0;
+            this.cbSensor1_1.Text = "1-1";
+            this.cbSensor1_1.UseVisualStyleBackColor = true;
+            // 
+            // cbSensor1_2
+            // 
+            this.cbSensor1_2.AutoSize = true;
+            this.cbSensor1_2.Location = new System.Drawing.Point(76, 272);
+            this.cbSensor1_2.Name = "cbSensor1_2";
+            this.cbSensor1_2.Size = new System.Drawing.Size(53, 19);
+            this.cbSensor1_2.TabIndex = 1;
+            this.cbSensor1_2.Text = "1-2";
+            this.cbSensor1_2.UseVisualStyleBackColor = true;
+            // 
+            // cbSensor2_2
+            // 
+            this.cbSensor2_2.AutoSize = true;
+            this.cbSensor2_2.Location = new System.Drawing.Point(489, 272);
+            this.cbSensor2_2.Name = "cbSensor2_2";
+            this.cbSensor2_2.Size = new System.Drawing.Size(53, 19);
+            this.cbSensor2_2.TabIndex = 2;
+            this.cbSensor2_2.Text = "2-2";
+            this.cbSensor2_2.UseVisualStyleBackColor = true;
+            // 
+            // cbSensor2_1
+            // 
+            this.cbSensor2_1.AutoSize = true;
+            this.cbSensor2_1.Location = new System.Drawing.Point(489, 333);
+            this.cbSensor2_1.Name = "cbSensor2_1";
+            this.cbSensor2_1.Size = new System.Drawing.Size(53, 19);
+            this.cbSensor2_1.TabIndex = 3;
+            this.cbSensor2_1.Text = "2-1";
+            this.cbSensor2_1.UseVisualStyleBackColor = true;
+            // 
+            // cbSensor4_2
+            // 
+            this.cbSensor4_2.AutoSize = true;
+            this.cbSensor4_2.Location = new System.Drawing.Point(489, 24);
+            this.cbSensor4_2.Name = "cbSensor4_2";
+            this.cbSensor4_2.Size = new System.Drawing.Size(53, 19);
+            this.cbSensor4_2.TabIndex = 4;
+            this.cbSensor4_2.Text = "4-2";
+            this.cbSensor4_2.UseVisualStyleBackColor = true;
+            // 
+            // cbSensor4_1
+            // 
+            this.cbSensor4_1.AutoSize = true;
+            this.cbSensor4_1.Location = new System.Drawing.Point(489, 85);
+            this.cbSensor4_1.Name = "cbSensor4_1";
+            this.cbSensor4_1.Size = new System.Drawing.Size(53, 19);
+            this.cbSensor4_1.TabIndex = 5;
+            this.cbSensor4_1.Text = "4-1";
+            this.cbSensor4_1.UseVisualStyleBackColor = true;
+            // 
+            // cbSensor3_2
+            // 
+            this.cbSensor3_2.AutoSize = true;
+            this.cbSensor3_2.Location = new System.Drawing.Point(76, 24);
+            this.cbSensor3_2.Name = "cbSensor3_2";
+            this.cbSensor3_2.Size = new System.Drawing.Size(53, 19);
+            this.cbSensor3_2.TabIndex = 7;
+            this.cbSensor3_2.Text = "3-2";
+            this.cbSensor3_2.UseVisualStyleBackColor = true;
+            // 
+            // cbSensor3_1
+            // 
+            this.cbSensor3_1.AutoSize = true;
+            this.cbSensor3_1.Location = new System.Drawing.Point(76, 85);
+            this.cbSensor3_1.Name = "cbSensor3_1";
+            this.cbSensor3_1.Size = new System.Drawing.Size(53, 19);
+            this.cbSensor3_1.TabIndex = 6;
+            this.cbSensor3_1.Text = "3-1";
+            this.cbSensor3_1.UseVisualStyleBackColor = true;
+            // 
+            // cbSensorS_2
+            // 
+            this.cbSensorS_2.AutoSize = true;
+            this.cbSensorS_2.Location = new System.Drawing.Point(300, 139);
+            this.cbSensorS_2.Name = "cbSensorS_2";
+            this.cbSensorS_2.Size = new System.Drawing.Size(55, 19);
+            this.cbSensorS_2.TabIndex = 9;
+            this.cbSensorS_2.Text = "S-2";
+            this.cbSensorS_2.UseVisualStyleBackColor = true;
+            // 
+            // cbSensorS_1
+            // 
+            this.cbSensorS_1.AutoSize = true;
+            this.cbSensorS_1.Location = new System.Drawing.Point(300, 200);
+            this.cbSensorS_1.Name = "cbSensorS_1";
+            this.cbSensorS_1.Size = new System.Drawing.Size(55, 19);
+            this.cbSensorS_1.TabIndex = 8;
+            this.cbSensorS_1.Text = "S-1";
+            this.cbSensorS_1.UseVisualStyleBackColor = true;
+            // 
+            // btnConveyor3
+            // 
+            this.btnConveyor3.BackColor = System.Drawing.Color.Lime;
+            this.btnConveyor3.Location = new System.Drawing.Point(135, 27);
+            this.btnConveyor3.Name = "btnConveyor3";
+            this.btnConveyor3.Size = new System.Drawing.Size(156, 77);
+            this.btnConveyor3.TabIndex = 10;
+            this.btnConveyor3.UseVisualStyleBackColor = false;
+            // 
+            // btnConveyor4
+            // 
+            this.btnConveyor4.BackColor = System.Drawing.Color.Lime;
+            this.btnConveyor4.Location = new System.Drawing.Point(548, 27);
+            this.btnConveyor4.Name = "btnConveyor4";
+            this.btnConveyor4.Size = new System.Drawing.Size(156, 77);
+            this.btnConveyor4.TabIndex = 11;
+            this.btnConveyor4.UseVisualStyleBackColor = false;
+            // 
+            // btnConveyorS
+            // 
+            this.btnConveyorS.BackColor = System.Drawing.Color.Lime;
+            this.btnConveyorS.Location = new System.Drawing.Point(361, 139);
+            this.btnConveyorS.Name = "btnConveyorS";
+            this.btnConveyorS.Size = new System.Drawing.Size(156, 77);
+            this.btnConveyorS.TabIndex = 12;
+            this.btnConveyorS.UseVisualStyleBackColor = false;
+            // 
+            // btnConveyor1
+            // 
+            this.btnConveyor1.BackColor = System.Drawing.Color.Lime;
+            this.btnConveyor1.Location = new System.Drawing.Point(135, 275);
+            this.btnConveyor1.Name = "btnConveyor1";
+            this.btnConveyor1.Size = new System.Drawing.Size(156, 77);
+            this.btnConveyor1.TabIndex = 13;
+            this.btnConveyor1.UseVisualStyleBackColor = false;
+            // 
+            // btnConveyor2
+            // 
+            this.btnConveyor2.BackColor = System.Drawing.Color.Lime;
+            this.btnConveyor2.Location = new System.Drawing.Point(548, 275);
+            this.btnConveyor2.Name = "btnConveyor2";
+            this.btnConveyor2.Size = new System.Drawing.Size(156, 77);
+            this.btnConveyor2.TabIndex = 14;
+            this.btnConveyor2.UseVisualStyleBackColor = false;
+            // 
+            // btnAuto
+            // 
+            this.btnAuto.Location = new System.Drawing.Point(155, 393);
+            this.btnAuto.Name = "btnAuto";
+            this.btnAuto.Size = new System.Drawing.Size(61, 28);
+            this.btnAuto.TabIndex = 15;
+            this.btnAuto.Text = "자동";
+            this.btnAuto.UseVisualStyleBackColor = true;
+            this.btnAuto.Click += new System.EventHandler(this.btnAuto_Click);
+            // 
+            // btnTakeOut
+            // 
+            this.btnTakeOut.Location = new System.Drawing.Point(254, 393);
+            this.btnTakeOut.Name = "btnTakeOut";
+            this.btnTakeOut.Size = new System.Drawing.Size(61, 28);
+            this.btnTakeOut.TabIndex = 16;
+            this.btnTakeOut.Text = "반출";
+            this.btnTakeOut.UseVisualStyleBackColor = true;
+            this.btnTakeOut.Click += new System.EventHandler(this.btnTakeOut_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(361, 393);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(61, 28);
+            this.btnStop.TabIndex = 17;
+            this.btnStop.Text = "정지";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnTakeIn
+            // 
+            this.btnTakeIn.Location = new System.Drawing.Point(456, 393);
+            this.btnTakeIn.Name = "btnTakeIn";
+            this.btnTakeIn.Size = new System.Drawing.Size(61, 28);
+            this.btnTakeIn.TabIndex = 18;
+            this.btnTakeIn.Text = "입고";
+            this.btnTakeIn.UseVisualStyleBackColor = true;
+            this.btnTakeIn.Click += new System.EventHandler(this.btnTakeIn_Click);
+            // 
+            // ConvMotionProc
+            // 
+            this.ConvMotionProc.Enabled = true;
+            this.ConvMotionProc.Interval = 1000;
+            this.ConvMotionProc.Tick += new System.EventHandler(this.ConvMotionProc_Tick);
+            // 
+            // MainSchedulerProc
+            // 
+            this.MainSchedulerProc.Tick += new System.EventHandler(this.MainSchedulerProc_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 433);
+            this.Controls.Add(this.btnTakeIn);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnTakeOut);
+            this.Controls.Add(this.btnAuto);
+            this.Controls.Add(this.btnConveyor2);
+            this.Controls.Add(this.btnConveyor1);
+            this.Controls.Add(this.btnConveyorS);
+            this.Controls.Add(this.btnConveyor4);
+            this.Controls.Add(this.btnConveyor3);
+            this.Controls.Add(this.cbSensorS_2);
+            this.Controls.Add(this.cbSensorS_1);
+            this.Controls.Add(this.cbSensor3_2);
+            this.Controls.Add(this.cbSensor3_1);
+            this.Controls.Add(this.cbSensor4_1);
+            this.Controls.Add(this.cbSensor4_2);
+            this.Controls.Add(this.cbSensor2_1);
+            this.Controls.Add(this.cbSensor2_2);
+            this.Controls.Add(this.cbSensor1_2);
+            this.Controls.Add(this.cbSensor1_1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox cbSensor1_1;
+        private System.Windows.Forms.CheckBox cbSensor1_2;
+        private System.Windows.Forms.CheckBox cbSensor2_2;
+        private System.Windows.Forms.CheckBox cbSensor2_1;
+        private System.Windows.Forms.CheckBox cbSensor4_2;
+        private System.Windows.Forms.CheckBox cbSensor4_1;
+        private System.Windows.Forms.CheckBox cbSensor3_2;
+        private System.Windows.Forms.CheckBox cbSensor3_1;
+        private System.Windows.Forms.CheckBox cbSensorS_2;
+        private System.Windows.Forms.CheckBox cbSensorS_1;
+        private System.Windows.Forms.Button btnConveyor3;
+        private System.Windows.Forms.Button btnConveyor4;
+        private System.Windows.Forms.Button btnConveyorS;
+        private System.Windows.Forms.Button btnConveyor1;
+        private System.Windows.Forms.Button btnConveyor2;
+        private System.Windows.Forms.Button btnAuto;
+        private System.Windows.Forms.Button btnTakeOut;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnTakeIn;
+        private System.Windows.Forms.Timer ConvMotionProc;
+        private System.Windows.Forms.Timer MainSchedulerProc;
+    }
+}
+
---
+++ GGAP/Conveyor/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conveyor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        struct PIO
+        {
+            public bool blsTrReq;
+            public bool blsBusy;
+            public bool blsCompt;
+            public bool blsUReq;
+            public bool blsLReq;
+            public bool blsReady;
+        }
+        PIO pioConv1, pioConv2, pioConv3, pioConv4;
+
+        bool statusCwConv1, statusCwConv2, statusCwConv3, statusCwConv4, statusCwConvS;
+        bool statusCcwConv1, statusCcwConv2, statusCcwConv3, statusCcwConv4, statusCcwConvS;
+
+        private void btnTakeIn_Click(object sender, EventArgs e)
+        {
+            blsTakeIn = true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTakeOut_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAuto_Click(object sender, EventArgs e)
+        {
+            blsAutoConv1 = false;
+            blsAutoConv2 = false;
+            blsAutoConv3 = false;
+            blsAutoConv4 = false;
+            blsAutoConvS = false;
+        }
+
+        bool ConvMotionBlink;
+        private void ConvMotionProc_Tick(object sender, EventArgs e)
+        {
+            if(ConvMotionBlink == true)
+            {
+                btnConveyor1.Text = "";
+                btnConveyor2.Text = "";
+                btnConveyor3.Text = "";
+                btnConveyor4.Text = "";
+                btnConveyorS.Text = "";
+                ConvMotionBlink = false;
+            }
+            else
+            {
+                if (!statusCwConv1 && !statusCcwConv1)
+                    btnConveyor1.Text = "";
+                else if (statusCwConv1)
+                    btnConveyor1.Text = "CW";
+                else if(statusCcwConv1)
+                    btnConveyor1.Text = "CCW";
+                if (!statusCwConv2 && !statusCcwConv2)
+                    btnConveyor2.Text = "";
+                else if (statusCwConv2)
+                    btnConveyor2.Text = "CW";
+                else if (statusCcwConv1)
+                    btnConveyor2.Text = "CCW";
+                if (!statusCwConv3 && !statusCcwConv3)
+                    btnConveyor3.Text = "";
+                else if (statusCwConv3)
+                    btnConveyor3.Text = "CW";
+                else if (statusCcwConv3)
+                    btnConveyor3.Text = "CCW";
+                if (!statusCwConv4 && !statusCcwConv4)
+                    btnConveyor4.Text = "";
+                else if (statusCwConv4)
+                    btnConveyor4.Text = "CW";
+                else if (statusCcwConv4)
+                    btnConveyor4.Text = "CCW";
+                if (!statusCwConvS && !statusCcwConvS)
+                    btnConveyorS.Text = "";
+                else if (statusCwConvS)
+                    btnConveyorS.Text = "CW";
+                else if (statusCcwConvS)
+                    btnConveyorS.Text = "CCW";
+
+                ConvMotionBlink = true;
+            }
+        }
+
+        int stepConv1, stepConv2, stepConv3, stepConv4, stepConvS;
+        int oldStepConv1, oldStepConv2, oldStepConv3, oldStepConv4, oldStepConvS;
+        int countConv1, countConv2, countConv3, countConv4, countConvS;
+
+        bool blsAutoConv1, blsAutoConv2, blsAutoConv3, blsAutoConv4, blsAutoConvS;
+        bool blsTakeIn, blsTakeOut;
+        private void MainSchedulerProc_Tick(object sender, EventArgs e)
+        {
+            ProcConv1();
+            ProcConv2();
+            ProcConv3();
+            ProcConv4();
+            ProcConvS();
+        }
+
+        void ProcConv1()
+        {
+
+        }
+
+        void ProcConv2()
+        {
+            if (blsAutoConv2 == true)
+            {
+                switch (stepConv2)
+                {
+                    case 0:
+                        pioConv2.blsUReq = false;
+                        pioConv2.blsLReq = false;
+                        pioConv2.blsReady = false;
+                        statusCwConv2 = false;
+                        statusCcwConv2 = false;
+
+                        stepConv2 = 100;
+                        break;
+
+                    case 100:
+                        if (cbSensor2_2.Checked == true)
+                        {
+                            stepConv2 = 200;
+                        }
+                        else
+                        {
+                            if (blsTakeIn == true)
+                            {
+                                stepConv2 = 110;
+                                countConv2 = 0;
+                            }
+                        }
+                        break;
+
+                    case 110:
+                        statusCcwConv2 = true;
+                        if (cbSensor2_2.Checked == true)
+                        {
+                            statusCcwConv2 = false;
+                            pioConv2.blsUReq = true;
+                            stepConv2 = 200;
+                        }
+                        break;
+
+                    case 200:
+                        if (cbSensor2_2.Checked == false)
+                        {
+                            stepConv2 = 100;
+                        }
+                        else if(pioConv2.blsTrReq)
+                        {
+                            stepConv2 = 210;
+                            pioConv2.blsReady = true;
+                        }
+                        break;
+
+                    case 210:
+                        if(pioConv2.blsBusy)
+                        {
+                            pioConv2.blsUReq = true;
+                            stepConv2 = 220;
+                        }
+                        break;
+
+                    case 220:
+                        statusCcwConv2 = true;
+                        if(!pioConv2.blsTrReq && !pioConv2.blsBusy && pioConv2.blsCompt)
+                        {
+                            pioConv2.blsUReq = false;
+                            statusCcwConv2 = false;
+                            stepConv2 = 230;
+                        }
+                        break;
+
+                    case 230:
+                        if (!pioConv2.blsCompt)
+                            stepConv2 = 100;
+                        break;
+
+                    default:
+                        stepConv2 = 0;
+                        break;
+                }
+            }
+            else
+            {
+                stepConv2 = 0;
+            }
+
+            if(oldStepConv2 != stepConv2)
+            {
+                Console.WriteLine($"Conveyor 2 Step = {stepConv2}");
+            }
+            oldStepConv2 = stepConv2;
+            blsTakeIn = false;
+        }
+        
+        void ProcConv3()
+        {
+
+        }
+
+        void ProcConv4()
+        {
+
+        }
+
+        void ProcConvS()
+        {
+
+        }
+    }
+}
